94cfc2a14951efc821118f849df76345051c9e63
Cooiut <zhaobochen1999@gmail.com>
2020-02-19 09:47:10 -0800

    Add new files

---
+++ main.py
@@ -0,0 +1,13 @@
+# 读文件
+file = "Test/a_example.in"
+# file = "Test/b_small.in"
+# file = "Test/c_medium.in"
+# file = "Test/d_quite_big.in"
+# file = "Test/e_also_big.in"
+
+f = open(file, "r")
+
+
+# 写文件
+
+
